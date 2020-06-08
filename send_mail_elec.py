9aa7247a747d786a7c03b977dc8b666a06f1b77b
centos <gyshao@insscn.com>
2020-06-08 09:39:18 +0800

    对电量采集send_mail_elec做容错处理，避免读到空数据发送失败报错

--- send_mail_elec.py
+++ send_mail_elec.py
@@ -1,10 +1,13 @@
 import  readconfig
 import  sql
 from f_email.sendemail import *
-
+import logdebug
 EMAILADDR = readconfig.readcon("fc_manual","emailaddr") #
 def elec_mail():
 
-
-    res = email(EMAILADDR,sql.select_elec_from_mysql())
+    try :
+        res = email(EMAILADDR,sql.select_elec_from_mysql())
+    except Exception as  err:
+        logdebug.logdeb(err)
+        return
     print(EMAILADDR,"send ok",res)
